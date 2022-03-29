f427a7ef7483f497f55fd0fb02d2b29710bd7a25
Saandeep Aathreya Sidhapur Lakshminarayan <saandeepaath@usf.edu>
2022-03-28 21:06:24 -0400

    training start

--- train.py
+++ train.py
@@ -7,6 +7,10 @@ from sys import exit as e
 import torch 
 from torch import nn, optim
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from sklearn.metrics import accuracy_score
+
 
 from args import get_args
 from dataset import OpenData
@@ -22,8 +26,30 @@ def seed_everything(seed):
   torch.cuda.manual_seed_all(seed)
   torch.backends.cudnn.deterministic = True
 
+
+def get_valScores(testloader, model):
+  accuracy_detail = {}
+  model.eval()
+  y_pred = []
+  y_true= []
+  for b, (x, target, sub) in enumerate(tqdm(testloader), 0):
+    accuracy_detail[sub] = {'pred': 0, 'true': 0}
+    x = x.to(device)
+    with torch.no_grad():
+      out = model(x)
+    y_true.append(target[0].item())
+    y_pred.append(torch.argmax(out, dim=1)[0].item())
+
+    accuracy_detail[sub]['pred'] = torch.argmax(out, dim=1)[0].item()
+    accuracy_detail[sub]['true'] = target[0].item()
+    
+  return accuracy_score(y_pred, y_true)
+
+
 if __name__ == "__main__":
   seed_everything(42)
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  torch.autograd.set_detect_anomaly(True)
 
   os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
   os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
@@ -36,6 +62,8 @@ if __name__ == "__main__":
   test_dataset = OpenData(args, "test")
   testloader = DataLoader(test_dataset, batch_size = args.batch, shuffle=True)
 
+  writer = SummaryWriter()
+
   print(f"Training isntances: {len(trainloader)}")
   print(f"Test instances: {len(testloader)}")
 
@@ -46,7 +74,10 @@ if __name__ == "__main__":
     d_model = 136
   elif args.feat == "lm3d":
     d_model = 204
+  
   model = Transformer(d_model, args.n_head, args.nx, args.n_class)
+  model = nn.DataParallel(model)
+  model = model.to(device)
 
   print("Total Parameters: ", sum(p.numel() for p in model.parameters()))
   print("Total Trainable Parameters: ", sum(p.numel() for p in model.parameters() if p.requires_grad))
@@ -55,8 +86,12 @@ if __name__ == "__main__":
   criterion = nn.NLLLoss()
 
   pbar = tqdm(range(args.iter))
+  train_acc = 0.0
   for epoch in pbar:
     for b, (x, target, fl) in enumerate(tqdm(trainloader), 0):
+      model.train()
+      x = x.to(device)
+      target = target.to(device)
       out = model(x)
 
       loss = criterion(out, target)
@@ -65,4 +100,16 @@ if __name__ == "__main__":
       loss.backward()
       optimizer.step()
 
-      pbar.set_description(f"epoch: {epoch}; loss: {round(loss.item(), 3)}")
+      if epoch+1 % 5 == 0:
+        pbar.set_description("Getting train accuracy")
+        train_acc = get_valScores(trainloader, model)
+
+      val_acc = get_valScores(testloader, model)
+
+      writer.add_scalar("Loss/Train", loss.item(), b*(epoch+1))
+      writer.add_scalar("Acc/Val", val_acc, b * (epoch+1))
+
+      pbar.set_description(f"epoch: {epoch}; Train loss: {round(loss.item(), 3)}; Val Acc: {val_acc}; Train Acc: {train_acc}")
+  
+  writer.flush()
+  writer.close()
